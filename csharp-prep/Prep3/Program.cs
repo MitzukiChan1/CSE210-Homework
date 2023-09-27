29a9e84a00cc98b75ba31f3273112c1eea84e07e
MitzukiChan1 <Mario.70887611@gmail.com>
2023-09-26 23:17:16 -0500

    w1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomgenerator = new Random();
+        int magicnumber = randomgenerator.Next(1,101);
+        int guess = -1;
+        while (guess != magicnumber)
+        {
+            Console.Write("Your Guess: ");
+            guess = int.Parse(Console.ReadLine());
+            if (magicnumber > guess)
+            {
+                Console.WriteLine("Higher");
+        
+            }
+            else if  (magicnumber < guess)
+            {
+            Console.WriteLine("lower");
+            }
+            else 
+            {
+                Console.WriteLine("Correct!");
+            }
+        }
     }
 }
